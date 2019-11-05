40068d63ea3144f028e67830aac00bd00bf37ea4
Devika Nair <dnair@fandm.edu>
2019-11-04 21:02:18 -0500

    finishing first go at pii table

--- R/search_addresses.R
+++ R/search_addresses.R
@@ -7,9 +7,15 @@ source("R/search_streets.R")
 search_addresses <- function(vec, output) {
   cities <- search_cities_in_states(vec, "df")
   streets <- search_streets(vec, "df") 
-  addresses <- inner_join(cities, streets, by = c("ID", "OriginalString")) %>% 
-    mutate(AddressYN = ifelse(CitiesYN == TRUE & StreetsYN == TRUE, TRUE, FALSE)) #,
-           #AddressString = ifelse(AddressYN == TRUE, c(tidyr::unnest(CitiesString), tidyr::unnest(StreetsString))))
+  if (any(cities$CitiesYN) & any(streets$StreetsYN)) {
+    addresses <- inner_join(cities, streets, by = c("ID", "OriginalString")) %>% 
+      mutate(AddressYN = ifelse(CitiesYN == TRUE & StreetsYN == TRUE, TRUE, FALSE)) 
+  }
+  else {
+    addresses <-  dplyr::tibble(OriginalString = vec, 
+                             AddressYN = FALSE,
+                             AddressString = NA)
+  }
   
   if (missing(output)||output == "vector") {
     return(addresses$AddressYN)
@@ -22,7 +28,7 @@ search_addresses <- function(vec, output) {
   else {
     print("Output argument invalid.")
   }
-   
+  
 }
 
-s
+
--- R/search_cities.R
+++ R/search_cities.R
@@ -3,23 +3,31 @@ library(dplyr)
 source("R/search_state.R")
 
 search_cities_in_states <- function(vec, output) {
-  statesY <- search_state(vec, "df") %>% 
-    filter(StatesYN == TRUE) %>% 
-    mutate(ID = seq.int(nrow(.))) %>% 
-    tidyr::unnest() %>% 
-    select(ID, OriginalString, StatesString) %>%
-    mutate(OriginalString = str_replace_all(OriginalString, "\n", ", "),
-           stringsearchbefore = str_extract(OriginalString, 
-                                            pattern = paste0("(?:\\w+\\W*){3}\\b", StatesString)))
+  states <- search_state(vec, "df") 
   
-  cityregex <- read.csv("data/cityregex.csv", colClasses = c("character", "character"))
-  statesY <- statesY %>% left_join(cityregex, by = c("StatesString" = "state")) 
-  cities <-  statesY %>% transmute(ID = ID, 
-                                   OriginalString = OriginalString, 
-                                   StatesString = StatesString, 
-                                   CitiesYN = str_detect(string = stringsearchbefore, pattern = pattern),
-                                   CitiesString = str_extract_all(string = stringsearchbefore, pattern = pattern)) 
-  
+  if (any(states$StatesYN)) {
+    statesY <- states %>%
+      filter(StatesYN == TRUE) %>% 
+      mutate(ID = seq.int(nrow(.))) %>%
+      tidyr::unnest(StatesString) %>%
+      select(ID, OriginalString, StatesString) %>%
+      mutate(OriginalString = str_replace_all(OriginalString, "\n", ", "),
+             stringsearchbefore = str_extract(OriginalString,
+                                              pattern = paste0("(?:\\w+\\W*){3}\\b", StatesString)))
+    
+    cityregex <- read.csv("data/cityregex.csv", colClasses = c("character", "character"))
+    statesY <- statesY %>% left_join(cityregex, by = c("StatesString" = "state"))
+    cities <-  statesY %>% transmute(ID = ID,
+                                     OriginalString = OriginalString,
+                                     StatesString = StatesString,
+                                     CitiesYN = str_detect(string = stringsearchbefore, pattern = pattern),
+                                     CitiesString = str_extract_all(string = stringsearchbefore, pattern = pattern)) 
+  }
+  else {cities <-  dplyr::tibble(OriginalString = vec, 
+                           StatesString = NA,
+                           CitiesYN = FALSE,
+                           CitiesString = NA) 
+  }
   
   if (missing(output)||output == "vector") {
     return(cities$CitiesYN)
@@ -32,5 +40,4 @@ search_cities_in_states <- function(vec, output) {
   else {
     print("Output argument invalid.")
   }
-  
 }
