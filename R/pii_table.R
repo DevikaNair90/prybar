dfe441b18740e75021e875ae6e358d65a3d28f23
Devika Nair <dnair@virginia.edu>
2019-11-06 14:34:50 -0500

    adding documentation to functions, pii_table

--- R/pii_table.R
+++ R/pii_table.R
@@ -1,13 +1,29 @@
-library(dplyr)
-library(stringr)
-
-setwd("Documents/GitHub/privaR/")
-
-source("R/search_addresses.R")
-source("R/search_dob.R")
-source("R/search_email.R")
-source("R/search_phone.R")
-source("R/search_ssn.R")
+#' Creates a summary table of PII for a datset.
+#'
+#' This function takes a dataframe and searches each column for presence of 
+#' any text that appears as PII: emails, DOB, SSNs, phone numbers, and 
+#' addresses (street address, city/states, zipcodes). 
+#'
+#' @param df A dataframe with (vector-able) variables.
+#' @import stringr
+#' @import dplyr
+#' @suggest generator::
+#' @export
+#' @examples
+#' testcase <- data.frame(name = generator::r_full_names(n),  
+#' snn = generator::r_national_identification_numbers(n), 
+#' dob = generator::r_date_of_births(n), 
+#' email = generator::r_email_addresses(n), 
+#' ip = generator::r_ipv4_addresses(n), 
+#' phone = generator::r_phone_numbers(n),  
+#' credit_card = generator::r_credit_card_numbers(n), 
+#' lat = generator::r_latitudes(n), 
+#' lon = generator::r_longitudes(n), 
+#' stringsAsFactors = FALSE)
+#' 
+devtools::load_all()
+#devtools::use_package(package = "stringr", type = )
+#devtools::use_package
 
 pii_table <- function(df) {
   df <- as.data.frame(df)
